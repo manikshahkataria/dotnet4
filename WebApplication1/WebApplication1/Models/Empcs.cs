a61d44e909eb1d7b35a5da76622e9ac660ef6945
manik <manikshah1998@gmail.com>
2020-12-03 16:08:42 +0530

    second

---
+++ WebApplication1/WebApplication1/Models/Empcs.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class Empcs
+    {
+        public int Eid { get; set; }
+        public string Ename { get; set; }
+        public string Edsign { get; set; }
+        public DateTime Edoj { get; set; }
+    }
+}
--- WebApplication1/WebApplication1/Program.cs
+++ WebApplication1/WebApplication1/Program.cs
@@ -6,13 +6,14 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-
+using WebApplication1.Models;
 namespace WebApplication1
 {
     public class Program
     {
         public static void Main(string[] args)
         {
+          
             CreateHostBuilder(args).Build().Run();
         }
 
